515cbbebe51f72d4cf115d804e043ba923ca58af
RendyTR <rendytr23@gmail.com>
2023-03-11 00:30:21 +0700

    Fix template

--- data/LINE/liff.py
+++ data/LINE/liff.py
@@ -40,28 +40,24 @@ class Liff(object):
 
     @loggedIn
     def sendFlexText(self, to, text):
-        main    = ["dark","red","cyan","yellow","green","white"]
-        color   = random.choice(main)
         xyz     = LiffChatContext(to)
         xyzz    = LiffContext(chat=xyz)
         view    = LiffViewRequest('1603264152-Bdymw76d', xyzz)
         token   = self.liff.issueLiffView(view)
         url     = 'https://api.line.me/message/v3/share'
         headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer %s' % token.accessToken }
-        data    = {'messages': [{'type': 'text', 'text': text, 'sentBy': {'label': '| IMJUSTGOOD.COM', 'iconUrl': f'https://imjustgood.com/img/{color}-logo.png', 'linkUrl': 'https://api.imjustgood.com' } }] }
+        data    = {'messages': [{'type': 'text', 'text': text, 'sentBy': {'label': ' | IMJUSTGOOD', 'iconUrl': 'https://i.ibb.co/XZ90vcP/justgood.png', 'linkUrl': 'https://api.imjustgood.com' } }] }
         return requests.post(url, headers=headers, data=json.dumps(data))
 
     @loggedIn
     def sendFlexImage(self, to, imageUrl, animated=False):
-        main    = ["dark","red","cyan","yellow","green","white"]
-        color   = random.choice(main)
         xyz     = LiffChatContext(to)
         xyzz    = LiffContext(chat=xyz)
         view    = LiffViewRequest('1603264152-Bdymw76d', xyzz)
         token   = self.liff.issueLiffView(view)
         url     = 'https://api.line.me/message/v3/share'
         headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer %s' % token.accessToken }
-        data    = {'messages': [{'type': 'image', 'originalContentUrl': imageUrl, 'previewImageUrl': imageUrl, 'animated': animated, 'extension': 'jpg', 'sentBy': {'label': '| IMJUSTGOOD.COM', 'iconUrl': f'https://imjustgood.com/img/{color}-logo.png', 'linkUrl': 'https://https://api.imjustgood.com' } }] }
+        data    = {'messages': [{'type': 'image', 'originalContentUrl': imageUrl, 'previewImageUrl': imageUrl, 'animated': animated, 'extension': 'jpg', 'sentBy': {'label': ' | IMJUSTGOOD', 'iconUrl': 'https://i.ibb.co/XZ90vcP/justgood.png', 'linkUrl': 'https://https://api.imjustgood.com' } }] }
         return requests.post(url, headers=headers, data=json.dumps(data))
 
     @loggedIn
@@ -77,12 +73,8 @@ class Liff(object):
 
     @loggedIn
     def sendFlexVideo(self, to, videoUrl, thumbnail=None):
-        color   = ["dark","red","cyan","yellow","green","white"]
         if thumbnail is None:
-           color   = random.choice(color)
-           thumbnail = f"https://imjustgood.com/img/{color}-logo.png"
-        if thumbnail in color:
-           thumbnail = f"https://imjustgood.com/img/{thumbnail}-logo.png"
+           thumbnail = 'https://i.ibb.co/YcR420B/justgood-thumbnail.jpg'
         xyz     = LiffChatContext(to)
         xyzz    = LiffContext(chat=xyz)
         view    = LiffViewRequest('1603264152-Bdymw76d', xyzz)
